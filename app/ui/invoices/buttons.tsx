0c971067d7fe5fb282c61ac8bf16e80b11c18d5d
Tung Nguyen Tu <tungnguyentu@bizflycloud.vn>
2025-01-13 11:15:09 +0700

    final2

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,7 +1,7 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deleteInvoice } from '@/app/lib/actions';
-
+import { useActionState } from 'react';
 export function CreateInvoice() {
   return (
     <Link
@@ -26,11 +26,13 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const initialState = { message: '' };
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
- 
+  const [, formAction] = useActionState(deleteInvoiceWithId, initialState);
+  
   return (
-    <form action={deleteInvoiceWithId}>
-      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+    <form action={formAction}>
+      <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-4" />
       </button>
